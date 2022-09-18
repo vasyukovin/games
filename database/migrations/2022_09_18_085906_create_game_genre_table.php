4f3968df7c936ce86032b617e1cf12676272384e
Igor Vasyukov <vasyukovin@yandex.ru>
2022-09-18 12:07:20 +0300

    добавил миграции

--- database/migrations/2022_09_18_085906_create_game_genre_table.php
+++ database/migrations/2022_09_18_085906_create_game_genre_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('game_genre', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
+            $table->foreignId('game_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('genre_id')->constrained()->cascadeOnDelete();
+            $table->unique(['game_id', 'genre_id']);
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('game_genre');
     }
 };
